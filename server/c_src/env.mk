0c1f5f57a8dcb38c2da95e235e0ecc4ad1e5f041
川 王 <wangchuan@chuandeMacBook-Air.local>
2016-02-06 00:20:25 +0800

    update

--- server/c_src/env.mk
+++
@@ -1,3 +0,0 @@
-ERTS_INCLUDE_DIR ?= /usr/local/Cellar/erlang/18.1/lib/erlang/erts-7.1/include/
-ERL_INTERFACE_INCLUDE_DIR ?= /usr/local/Cellar/erlang/18.1/lib/erlang/lib/erl_interface-3.8/include
-ERL_INTERFACE_LIB_DIR ?= /usr/local/Cellar/erlang/18.1/lib/erlang/lib/erl_interface-3.8/lib
