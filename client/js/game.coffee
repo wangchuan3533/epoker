9c3500b1b137c00032705d48214419f4d4285352
wangchuan <wangchuan3533@gmail.com>
2016-03-13 22:02:45 +0800

    sync

--- client/js/game.coffee
+++ client/js/game.coffee
@@ -7,8 +7,24 @@ preload = ->
 
 create = ->
   game.add.sprite 0, 0, 'sky'
-  star = game.add.sprite 100, 100, 'star'
+  star = game.add.sprite 400, 300, 'star'
 
 update = ->
+  star.x = 800 * Math.random()
+  star.y = 600 * Math.random()
 
 game = new Phaser.Game 800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update}
+
+
+ws = new WebSocket 'ws://127.0.0.1:8080/echo'
+ws.onopen = ->
+  console.log 'websocket connected'
+
+ws.onmessage = (evt) ->
+  console.log 'Received message ' + evt.data
+  
+ws.onclose = ->
+  console.log 'websocket closed'
+
+window.test = (data) ->
+  ws.send JSON.stringify data
--- client/js/game.js
+++ client/js/game.js
@@ -1,23 +1,47 @@
-var create, game, preload, star, update;
+(function() {
+  var create, game, preload, star, update, ws;
 
-game = null;
+  game = null;
 
-star = null;
+  star = null;
 
-preload = function() {
-  game.load.image('sky', 'assets/sky.png');
-  return game.load.image('star', 'assets/star.png');
-};
+  preload = function() {
+    game.load.image('sky', 'assets/sky.png');
+    return game.load.image('star', 'assets/star.png');
+  };
 
-create = function() {
-  game.add.sprite(0, 0, 'sky');
-  return star = game.add.sprite(100, 100, 'star');
-};
+  create = function() {
+    game.add.sprite(0, 0, 'sky');
+    return star = game.add.sprite(400, 300, 'star');
+  };
 
-update = function() {};
+  update = function() {
+    star.x = 800 * Math.random();
+    return star.y = 600 * Math.random();
+  };
 
-game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-  preload: preload,
-  create: create,
-  update: update
-});
+  game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+    preload: preload,
+    create: create,
+    update: update
+  });
+
+  ws = new WebSocket('ws://127.0.0.1:8080/echo');
+
+  ws.onopen = function() {
+    return console.log('websocket connected');
+  };
+
+  ws.onmessage = function(evt) {
+    return console.log('Received message ' + evt.data);
+  };
+
+  ws.onclose = function() {
+    return console.log('websocket closed');
+  };
+
+  window.test = function(data) {
+    return ws.send(JSON.stringify(data));
+  };
+
+}).call(this);
